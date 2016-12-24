b89388f70cdeb6c3da62714aab8b87310eb4d0d4
Paul Trott <paultrott78@gmail.com>
2016-12-23 21:40:32 -0600

    Added data to view

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,6 +1,7 @@
 class FishController < ApplicationController
 
   def index
-    @fishes = [ "Yellowfin Tuna", "Blue Marlin", "Yellowmouth Grouper" ]
+    @fishes = Fish.all.order(:name)
+    @fish_with_regulations = Fish.where("minimum_size != 'no minimum' OR bag_limit !=  'no limit'")
   end
 end
