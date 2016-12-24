0e058854ece694d7e9d3938efdf684e2518f69bf
Paul Trott <paultrott78@gmail.com>
2016-12-24 15:47:10 -0600

    Added a fish show page, moved details to show page, added helper for displaying fish information, added show action

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,7 +1,10 @@
 class FishController < ApplicationController
 
   def index
-    @fishes = Fish.all.order(:name)
-    @fish_with_regulations = Fish.where("minimum_size != 'no minimum' OR bag_limit !=  'no limit'")
+    @fish_with_regulations = Fish.where("minimum_size != 'no minimum' OR bag_limit !=  'no limit'").order(:name)
+  end
+
+  def show
+    @fish = Fish.find(params[:id])
   end
 end
--- app/helpers/fish_helper.rb
+++ app/helpers/fish_helper.rb
@@ -1,2 +1,15 @@
 module FishHelper
+
+  def google_image_search_path(fish)
+    fish_param = { q: fish.species, tbm: "isch" }.to_param
+    "https://www.google.com/search?#{fish_param}"
+  end
+
+  def title(fish)
+    if fish.name.downcase == fish.alt_name.downcase
+      content_tag :h1, fish.name.capitalize
+    else
+      content_tag :h1, "#{fish.name.capitalize} (#{fish.alt_name.downcase})"
+    end
+  end
 end
