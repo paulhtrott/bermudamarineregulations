e7df985ac5df1ccb32cee0592acb5989c65ba780
Paul Trott <paultrott78@gmail.com>
2016-12-29 21:42:56 -0600

    refactored fish controller and moved querying to class scopes, refactored views to usean instance method for image path, added a custom scss file to be used for bootstrap overrides

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,7 +1,7 @@
 class FishController < ApplicationController
 
   def index
-    @fish_with_regulations = Fish.where("minimum_size != 'no minimum' OR bag_limit !=  'no limit'").order(:name)
+    @fish_with_regulations = Fish.fish_with_regulations.order_by_name
   end
 
   def show
--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,2 +1,9 @@
 class Fish < ApplicationRecord
+
+  scope :fish_with_regulations, -> { where("minimum_size != ? OR bag_limit != ?", "no minimum", "no limit") }
+  scope :order_by_name, -> { order(:name) }
+
+  def img_path
+    "marine/fish/#{name.downcase.gsub(/\s+/, '')}.jpg"
+  end
 end
